b232c3cdd27e9476683c9783cfe9168d7956e204
KuToo <183220614@qq.com>
2018-07-12 10:18:31 +0800

    kutoo

--- Application/Admin/Controller/HomeController.class.php
+++ Application/Admin/Controller/HomeController.class.php
@@ -4,12 +4,12 @@ use Think\Controller;
 
 class HomeController extends Controller
 {
-    public function __construct()
-    {
-        parent::__construct();
-        if (!session('admin_id')) {
-            //请先登录
-            $this->error('请先登录',U('/Admin/Login/login'));
-        }
-    }
+//    public function __construct()
+//    {
+//        parent::__construct();
+//        if (!session('admin_id')) {
+//            //请先登录
+//            $this->error('请先登录',U('/Admin/Login/login'));
+//        }
+//    }
 }
